745fe1738c669ad0705e30cc107243ed4508bbfa
Jared Lunde <jared.lunde@gmail.com>
2020-05-30 10:39:06 -0600

    Update tsconfig and build

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
-import React, {cloneElement} from 'react'
+import * as React from 'react'
 import {useKeycodes} from '@accessible/use-keycode'
 import useMergedRef from '@react-hook/merged-ref'
 
-export interface LinkProps {
-  children: React.ReactElement | JSX.Element
-}
-
 const Link: React.FC<LinkProps> = ({children}) => {
   const props = children.props
-  const onClick = props.onClick ? props.onClick : noop
 
-  return cloneElement(children, {
+  return React.cloneElement(children, {
     role: props.hasOwnProperty('role') ? props.role : 'link',
     tabIndex: props.hasOwnProperty('tabIndex') ? props.tabIndex : 0,
     ref: useMergedRef(
@@ -18,7 +13,7 @@ const Link: React.FC<LinkProps> = ({children}) => {
       children.ref,
       useKeycodes({
         // enter
-        13: onClick,
+        13: props.onClick ? props.onClick : noop,
       })
     ),
   })
@@ -27,9 +22,8 @@ const Link: React.FC<LinkProps> = ({children}) => {
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 const noop = () => {}
 
-export default Link
-
-/* istanbul ignore next */
-if (typeof process !== 'undefined' && process.env.NODE_ENV !== 'production') {
-  Link.displayName = 'AccessibleLink'
+export interface LinkProps {
+  children: React.ReactElement | JSX.Element
 }
+
+export default Link
---
+++ types/index.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react'
+declare const Link: React.FC<LinkProps>
+export interface LinkProps {
+  children: React.ReactElement | JSX.Element
+}
+export default Link
